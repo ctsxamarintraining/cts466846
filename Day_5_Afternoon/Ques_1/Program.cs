87242d242a9a95bc51cf3f89b55c85cadbd3a090
cts466846 <subin.v@cognizant.com>
2015-10-16 16:28:12 +0530

    Conditional Attributes

---
+++ Day_5_Afternoon/Ques_1/Program.cs
@@ -0,0 +1,30 @@
+﻿#define HELLO
+#define BYE
+using System;
+using System.Diagnostics;
+
+
+namespace Ques_1
+{
+	class MainClass
+	{
+
+		[Conditional("HELLO")]  
+		void hello() { 
+			Console.WriteLine("Welcome and Hello Everyone "); 
+		} 
+
+		[Conditional("BYE")]  
+		void bye() { 
+			Console.WriteLine("Bye See you Soon......"); 
+		} 
+
+		public static void Main (string[] args)
+		{
+			MainClass mainClass = new MainClass(); 
+
+			mainClass.hello ();
+			mainClass.bye ();
+		}
+	}
+}
